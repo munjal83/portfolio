ff96b6820d1a97a94a896b979236c329359352d5
munjal83 <munjalbpatel@hotmail.com>
2015-09-04 07:16:54 +0000

    changed the cover image of homepage and added some text in about page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/pages/portfolio' => 'pages#portfolio'
   get '/pages/about' => 'pages#about'
   get '/pages/contact' => 'pages#contact'
-  
+ get '/pages/resume' => 'pages#resume'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
